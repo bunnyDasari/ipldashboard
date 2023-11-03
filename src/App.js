9887672f8b82aa1f1304763a77734ab556ee121b
bunnyDasari <rohandasari098@gmail.com>
2023-11-03 22:11:22 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import props from "react";
+
+const MyApp = () =>{
+  const{name} = props
+  return(
+    <h1>{name}</h1>
+  )
+}
+export default MyApp;
---
+++ src/ren.js
@@ -0,0 +1,8 @@
+import React,{Component} from "react";
+import MyApp from './App'
+class MyClass extends Component{
+    render(){
+        return <MyApp name = "hello World" />
+    }
+}
+export default MyClass
